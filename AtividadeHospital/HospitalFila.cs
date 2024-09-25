ab9fd69e6e5bbc7a0fe1d783f92fea5ce1a6bafa
Mariana Fernandes <163003738+marivwz@users.noreply.github.com>
2024-09-25 10:21:13 -0300

    adição de método de cadastro e de pacientes prioritários

--- AtividadeHospital/HospitalFila.cs
+++ AtividadeHospital/HospitalFila.cs
@@ -7,6 +7,57 @@ using System.Threading.Tasks;
 namespace AtividadeHospital
 {
     internal class HospitalFila
-    {
+    {        public Paciente[] filaPacientes = new Paciente[10];
+        public int fimFila = 0;
+
+        public void CadastrarPaciente(Paciente paciente)
+        {
+            if (fimFila < filaPacientes.Length)
+            {
+                filaPacientes[fimFila] = paciente;
+                fimFila++;
+                Console.WriteLine($"Paciente {paciente.Nome} cadastrado com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Fila cheia! Não é possível cadastrar mais pacientes.");
+            }
+        }
+        public Paciente[] filaPacientes = new Paciente[10];
+        public int fimFila = 0;
+
+        public void CadastrarPaciente(Paciente paciente)
+        {
+            if (fimFila < filaPacientes.Length)
+            {
+                filaPacientes[fimFila] = paciente;
+                fimFila++;
+                Console.WriteLine($"Paciente {paciente.Nome} cadastrado com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Fila cheia! Não é possível cadastrar mais pacientes.");
+            }
+        }
+
+        public void IncluirPacientePrioritario(Paciente paciente)
+        {
+            if (fimFila < filaPacientes.Length)
+            {
+                for (int i = fimFila; i > 0; i--)
+                {
+                    filaPacientes[i] = filaPacientes[i - 1];
+                }
+                filaPacientes[0] = paciente;
+                fimFila++;
+                Console.WriteLine($"Paciente prioritário {paciente.Nome} foi incluído no início da fila.");
+            }
+            else
+            {
+                Console.WriteLine("Fila cheia! Não é possível incluir mais pacientes.");
+            }
+        }
     }
 }
+    
+
