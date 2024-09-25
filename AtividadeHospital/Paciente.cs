839fcc347348006778efa576a9423afd08ed07af
Mariana Fernandes <163003738+marivwz@users.noreply.github.com>
2024-09-25 10:11:27 -0300

    Adicionei métodos e parametros na classe paciente

--- AtividadeHospital/Paciente.cs
+++ AtividadeHospital/Paciente.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace AtividadeHospital
 {
     internal class Paciente
-    {
+    {        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string RgCpf { get; set; }
+
+        public Paciente(string nome, string endereco, string rgCpf)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            RgCpf = rgCpf;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Endereço: {Endereco}, RG/CPF: {RgCpf}";
+        }
     }
+    
 }
+
+    
+
